11425886345e56727b3c40071ed9213fe2221772
Petra Fisher <petra.fshr@gmail.com>
2024-05-14 00:01:07 +1200

    Revert to shell script

--- src/python/animate_output.py
+++ src/python/animate_output.py
@@ -10,7 +10,6 @@ netCDF file.
 import os
 import getpass
 import warnings
-import subprocess
 
 import numpy as np
 
@@ -123,59 +122,8 @@ for it1 in range(4, len(time) + 1, 4):
     plt.suptitle(f"t={time[it]/86400:.2f} days", fontsize=8, y=0.25)
     plt.savefig(f"../../output/frames/frame{ITER:03d}.png", format="png", dpi=300)
 
-
-def safe_ffmpeg_call(args):
-    """
-    Execute an ffmpeg command safely.
-
-    This function validates the command to be executed against a list of allowed commands
-    before invoking subprocess.run() to execute it.
-
-    Parameters:
-        args (list): A list containing the command and its arguments.
-
-    Raises:
-        ValueError: If the command is not in the list of allowed commands.
-    """
-    # List of allowed ffmpeg commands
-    allowed_commands = ["ffmpeg"]
-
-    # Check if the command is in the allowed list
-    if args[0] not in allowed_commands:
-        raise ValueError("Unsafe command detected.")
-
-    # Execute the command
-    subprocess.run(args, check=True)
-
-
-# Call ffmpeg to create mp4 animation
-safe_ffmpeg_call(
-    [
-        "ffmpeg",
-        "-r",
-        "5",
-        "-f",
-        "image2",
-        "-i",
-        "../../output/frames/frame%03d.png",
-        "-vframes",
-        "34",
-        "-vcodec",
-        "libx264",
-        "-crf",
-        "25",
-        "-pix_fmt",
-        "yuv420p",
-        "../../output/animations/animation.mp4",
-    ]
+os.system(
+    "ffmpeg -r 5 -f image2  -i ../../output/frames/frame%03d.png -vframes 34 -vcodec libx264\
+          -crf 25 -pix_fmt yuv420p ../../output/animations/animation.mp4"
 )
-
-# Call ffmpeg to convert mp4 to gif
-safe_ffmpeg_call(
-    [
-        "ffmpeg",
-        "-i",
-        "../../output/animations/animation.mp4",
-        "../../output/animations/animation.gif",
-    ]
-)
+os.system("ffmpeg -i ../../output/animations/animation.mp4  ../../output/animations/animation.gif")
