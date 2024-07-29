994a82a76b8f64d9ca69bb9cdc086a5b3eea3e87
Robert Van Gorder <petra.fshr@gmail.com>
2024-07-29 15:41:52 +1200

    Force using 8 processes and add some double quotes

--- src/shell_scripts/run_sims.sh
+++ src/shell_scripts/run_sims.sh
@@ -17,25 +17,22 @@ for (( i=0;i<$VEL_ELEMENTS;i++)); do
     sed -e "s|output.nc|/output_ujet_${VEL_ARRAY[${i}]}.nc|" ../config/namelist.in > ../config/namelist.tmp
 	sed -e "s/u_jet=12./u_jet=${VEL_ARRAY[${i}]}/" ../config/namelist.tmp > ../config/namelist.run	
  			
-    if [ -z "$1" ]
-    then
-        mpiexec -n 1 ./main.exe ../config/namelist.run > /tmp/std.out
-    else
-                mpiexec -n $1 ./main.exe ../config/namelist.run > /tmp/std.out
-    fi
-    rm ../config/namelist.tmp
-    mv /tmp/output_ujet_${VEL_ARRAY[${i}]}.nc ../tests/output.nc
+    mpiexec -n 8 ./main.exe ../config/namelist.run > /tmp/std.out
+    rm "../config/namelist.tmp"
+    mv "/tmp/output_ujet_${VEL_ARRAY[${i}]}.nc" "../tests/output.nc"
 
     # clear old animations and create new ones
+    echo "Creating animations"
     cd python && python3 animate_output.py -y
-    mv ../../output/animations/animation.gif ../../output/animations/animation_ujet_${VEL_ARRAY[${i}]}.gif 
-    mv ../../output/animations/animation.mp4 ../../output/animations/animation_ujet_${VEL_ARRAY[${i}]}.mp4 
+    mv "../../output/animations/animation.gif" "../../output/animations/animation_ujet_${VEL_ARRAY[${i}]}.gif" 
+    mv "../../output/animations/animation.mp4" "../../output/animations/animation_ujet_${VEL_ARRAY[${i}]}.mp4" 
 
     # create height and streamlines figure
+    echo "Creating height and streamlines figure"
     python3 height_and_streamlines.py
-    mv ../../output/frames/frame.png ../../output/streamlines/streamlines_ujet_${VEL_ARRAY[${i}]}.gif 
+    mv "../../output/frames/frame.png" "../../output/streamlines/streamlines_ujet_${VEL_ARRAY[${i}]}.gif" 
 
     # rename output.nc file for storage
-    mv ../../tests/output.nc ../../tests/output_ujet_${VEL_ARRAY[${i}]}.nc
+    mv "../../tests/output.nc" "../../tests/output_ujet_${VEL_ARRAY[${i}]}.nc"
     cd ..
 done
