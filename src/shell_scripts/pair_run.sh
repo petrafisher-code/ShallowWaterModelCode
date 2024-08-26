d9707c6e36a49f12cd12c300e4ab116d826d56fd
Petra Fisher <73986146+petrafisher-code@users.noreply.github.com>
2024-08-27 11:06:34 +1200

    Fixes for the theta x polar vortex pair run (#32)
    * Fixes for the theta x polar vortex pair run
    
    * Fixing line which is too long

--- src/shell_scripts/pair_run.sh
+++ src/shell_scripts/pair_run.sh
@@ -7,17 +7,19 @@
 # NAME2="grav"
 # ARRAY2=(1344 1368 1392 1416 1440 1464 1488 1512 1536 1560 1584 1608 1632 1656 1680 1704 1728 1752 1776 1800 1824) 
 
-# NAME1="nlat_thresh"
+NAME1="nlat_thresh"
 ARRAY1=(86.5 86.6 86.7 86.8 86.9 87.0 87.1 87.2 87.3 87.4 87.5 87.6 87.7 87.8 87.9 88.0)
 
-# NAME2="u_vortex"
-ARRAY2=(0. 1. 2. 3. 4. 5. 6. 7. 8. 9. 10. 11. 12. 13. 14. 15.)
+NAME2="u_vortex"
+ARRAY2=(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15)
 
 ELEMENTS1=${#ARRAY1[@]} # elements in first array
 ELEMENTS2=${#ARRAY2[@]} # elements in second array
 
-cd ..
-make
+cd .. && make
+
+# rm ../output/animations/animation.gif
+# rm ../output/animations/animation.mp4
 
 for (( i=0; i<ELEMENTS1; i++)); do
 	for (( j=0; j<ELEMENTS2; j++)); do
@@ -36,10 +38,21 @@ for (( i=0; i<ELEMENTS1; i++)); do
             # prepare to run python files
             cd python 
 
+            # # clear old animations and create new ones
+            # echo "Creating animations"
+            # python3 animate_output.py -y
+            # mv "../../output/animations/animation.gif" "../../output/animations/animation_${NAME1}_${ARRAY1[${i}]}_${NAME2}_${ARRAY2[${j}]}.gif" 
+            # mv "../../output/animations/animation.mp4" "../../output/animations/animation_${NAME1}_${ARRAY1[${i}]}_${NAME2}_${ARRAY2[${j}]}.mp4" 
+
             # create height and streamlines figure
             echo "Creating height and streamlines figure"
             python3 height_and_streamlines.py
             mv "../../output/frames/frame.png" "../../output/streamlines/streamlines_${NAME1}_${ARRAY1[${i}]}_${NAME2}_${ARRAY2[${j}]}.png" 
+
+            # create height and streamlines figure
+            echo "Creating velocity latitude figure"
+            python3 velocity_latitudes.py
+            mv "../../output/velocities/velocity_latitudes.png" "../../output/velocities/velocity_latitudes_${NAME1}_${ARRAY1[${i}]}_${NAME2}_${ARRAY2[${j}]}.png"
 
             # rename output.nc file for storage
             mv "../../tests/output.nc" "../../tests/output_${NAME1}_${ARRAY1[${i}]}_${NAME2}_${ARRAY2[${j}]}.nc"
